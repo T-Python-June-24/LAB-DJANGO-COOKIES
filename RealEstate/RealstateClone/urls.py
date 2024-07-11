e1712923f63d3225b033a05a95952ab777658b4f
e7gx <abalgabo@gmail.com>
2024-07-12 00:04:53 +0300

    added two pages-contactus,Properties with the it's style🎨....

--- RealEstate/RealstateClone/urls.py
+++ RealEstate/RealstateClone/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 app_name = 'RealstateClone'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('properties/',views.propertiesPage,name='properties'),
+    path('contact/',views.contact,name='contact')
 ]
--- RealEstate/RealstateClone/views.py
+++ RealEstate/RealstateClone/views.py
@@ -3,3 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'RealstateClone/home.html')
+
+
+properties = [
+    {"title": "Villa Modern in Malqa", "image": "villa1.jpg"},
+    {"title": "Great home for you in Rimal", "image": "villa2.jpg"},
+    {"title": "Villa with 8 bedrooms in Swedey", "image": "villa3.jpg"},
+    {"title": "Amazing Villa in Hitten", "image": "villa4.jpg"},
+
+
+]
+
+# Create your views here.
+def propertiesPage(request):
+    context = {
+        'properties': properties,
+    }
+    return render(request, 'RealstateClone/properties.html', context)
+
+
+def contact(request):
+    return render(request,'RealstateClone/contact.html')
+
