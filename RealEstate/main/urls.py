8cceca65969a0662c9355b8745cbc502f23c94ee
amira alrshidi <ameerah.alrashidi0111@gmail.com>
2024-07-13 00:22:29 +0300

    all pages are done with dynamic properites

--- RealEstate/main/urls.py
+++ RealEstate/main/urls.py
@@ -6,5 +6,6 @@ app_name = 'main'
 urlpatterns = [
     path('',views.home_page,name='home_page'),
     path('contact/',views.contact_page,name='contact_page'),
+    path('properties/',views.properties_page,name='properties_page')
 ]
 
--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -8,3 +8,17 @@ def home_page(request:HttpRequest):
 def contact_page(request:HttpRequest):
     
     return render(request,'main/contact.html')
+
+
+def properties_page(request:HttpRequest):
+
+    properties = [
+        {"title" : "Villa Modern in Malqa", "image" : "images/villa1.jpg"},
+        {"title" : "Great home for you in Rimal", "image" : "images/villa2.jpg"},
+        {"title" : "Villa with 8 bedrooms in Swedey", "image" : "images/villa3.jpg"},
+        {"title" : "Amazing Villa in Hitten", "image" : "images/villa4.jpg"},
+    ]
+    
+    response = render(request,'main/properties.html',{'properties_list':properties})
+
+    return response
