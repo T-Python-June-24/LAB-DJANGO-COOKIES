104980ee6d016b18c8264d7b57ebe262c8c4ef52
Sarah <monifsarah@gmail.com>
2024-07-12 15:56:04 +0300

    completed properties

--- RealEstate/main/urls.py
+++ RealEstate/main/urls.py
@@ -6,4 +6,6 @@ app_name = "main"
 urlpatterns = [
     path("", views.home_page, name="home_page"),
     path("contact/",views.contact_page,name="contact_page"),
+    path("property_list/",views.property_list,name="property_list"),
+
 ]
--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -5,3 +5,12 @@ def home_page(request):
     return render(request, "main/index.html")
 def contact_page(request):
     return render(request, "main/contact.html")
+
+def property_list(request):
+    properties = [
+        {"title": "Villa Modern in Malqa", "image": "images/villa1.jpg"},
+        {"title": "Great home for you in Rimal", "image": "images/villa2.jpg"},
+        {"title": "Villa with 8 bedrooms in Swedey", "image": "images/villa3.jpg"},
+        {"title": "Amazing Villa in Hitten", "image": "images/villa4.jpg"},
+    ]
+    return render(request, 'main/properties.html', {'properties': properties})
