2d84037f4585fedb969ed8795b4a2560eddc797c
Abdulrahman-Alwagdani <167430546+Abdulrahman-Alwagdani@users.noreply.github.com>
2024-07-12 15:43:46 +0300

    Completed the project structure

---
+++ RealEstate/main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.home_view, name='home_view'),
+    path('properties/', views.properties_view, name='properties_view'),
+    path('contact/', views.contact_view, name='contact_view'),
+]
