ab664a9b4392f5f0c81cd13a655754bac4cd7765
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-07-12 22:52:01 +0300

    create home page

--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 from django.http import HttpRequest,HttpResponse
 
 def home_view(request:HttpRequest):
-    return render(request, "home.html")
+
+    print(request.COOKIES)
+    
+    response = render(request, "home.html")
+    response.set_cookie("Abdulaziz", "The best Thinker")
+
+    return response
 
 def properties_view(request:HttpRequest):
-    pass
+    return render(request, "properties.html")
 
 def contact_view(request:HttpRequest):
-    pass
+    return render(request, "contact.html")
 
 
 
