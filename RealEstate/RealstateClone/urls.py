c4ccae2e7ccfb660d60735fee95df68a2b6c08f1
e7gx <abalgabo@gmail.com>
2024-07-13 02:13:50 +0300

    the project is ready 🎉

--- RealEstate/RealstateClone/urls.py
+++ RealEstate/RealstateClone/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 
 app_name = 'RealstateClone'
 urlpatterns = [
-    
     path('', views.home, name='home'),
-    path('properties/',views.propertiesPage,name='properties'),
-    path('contact/',views.contact,name='contact'),
+    path('properties/', views.propertiesPage, name='properties'),
+    path('contact/', views.contact, name='contact'),
     path('home_light/', views.home_light, name='home_light'),
     path('home_dark/', views.home_dark, name='home_dark'),
-    
+    path('properties_light/', views.propertiesPage_light, name='properties_light'),
+    path('properties_dark/', views.propertiesPage_dark, name='properties_dark'),
 ]
--- RealEstate/RealstateClone/views.py
+++ RealEstate/RealstateClone/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-
+from django.http import HttpRequest,HttpResponse
 
 
 properties = [
@@ -11,45 +11,49 @@ properties = [
 
 ]
 # Create your views here.
-def home(request):
+def home(request:HttpResponse):
     return render(request, 'RealstateClone/home.html')
 
 
 
-def propertiesPage(request):
+def propertiesPage(request:HttpResponse):
     context = {
         'properties': properties,
     }
     return render(request, 'RealstateClone/properties.html', context)
 
 
-def contact(request):
+def contact(request:HttpResponse):
     return render(request,'RealstateClone/contact.html')
 
 
-def home_light(request):
+def home_light(request:HttpResponse):
     response = redirect('RealstateClone:home')
     response.set_cookie('mode', 'dark', max_age=-3600)
     return response
 
-def home_dark(request):
+def home_dark(request:HttpResponse):
     response = redirect('RealstateClone:home')
     response.set_cookie('mode', 'dark', max_age=60*60*24*365*2)
     return response
 
 
-# def propertiesPage_dark(request):
-#     response = redirect('RealstateClone:propertiesPage')
-#     response.set_cookie('dark', 'mode', max_age=60*60*24*365*2)
-#     return response
 
-# def contact_light(request):
-#     response = redirect('RealstateClone:contact')
-#     response.set_cookie('light', 'mode', max_age=-1000)
-#     return response
+def propertiesPage_light(request:HttpResponse):
+    context = {
+        'properties': properties,
+    }
+    
+    response = render(request, 'RealstateClone/propertiesPage.html', context)
+    response.set_cookie('mode', 'light', max_age=60*60*24*365*2)
+    return response
 
+def propertiesPage_dark(request:HttpResponse):
+    context = {
+        'properties': properties,
+    }
+    
+    response = render(request, 'RealstateClone/propertiesPage.html', context)
+    response.set_cookie('mode', 'dark', max_age=60*60*24*365*2)
+    return response
 
-# def contact_dark(request):
-#     response = redirect('RealstateClone:contact')
-#     response.set_cookie('dark', 'mode', max_age=60*60*24*365*2)
-#     return response
