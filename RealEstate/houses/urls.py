445bd92f3b05aaa5e249c37c7bac29487e400c35
D. F <ifonei@hotmail.com>
2024-07-14 08:37:57 +0300

    REALESTATE WEBSITE DONE

---
+++ RealEstate/houses/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views 
+
+appname = 'houses'
+urlpatterns = [
+    path('',views.home, name = 'home'),
+    path('properties/',views.properties, name= 'properties'),
+    path('darkModeView',views.darkModeView, name= 'darkModeView'),
+    path('lightModeView',views.lightModeView, name= 'lightModeView'),
+]
+
+
---
+++ RealEstate/houses/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+# Create your views here.
+
+
+
+def home(request: HttpRequest):
+    response = render(request, 'houses/home.html')
+    response.set_cookie('name','value')
+    return response
+
+def properties(request: HttpRequest):
+    properties = [
+        {"title" : "Villa Modern in Malqa", "image" : "villa1.jpg"},
+        {"title" : "Great home for you in Rimal", "image" : "villa2.jpg"},
+        {"title" : "Villa with 8 bedrooms in Swedey", "image" : "villa3.jpg"},
+        {"title" : "Amazing Villa in Hitten", "image" : "villa4.jpg"},
+    ]
+    return render(request, 'houses/properties.html', context={'properties': properties})
+
+
+def darkModeView(request:HttpRequest):
+    response = redirect("houses:home")
+    response.set_cookie("mode","dark",max_age=60*60*24)
+    return response
+
+def lightModeView(request:HttpRequest):
+    response = redirect("houses:home")
+    response.set_cookie("mode","dark",max_age= -3600)
+    return response
