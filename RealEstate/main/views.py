59fd12d3dd732f93f755c62ce835615079fd6e59
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-07-14 08:34:30 +0300

    add context

--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -6,14 +6,22 @@ def home_view(request:HttpRequest):
     print(request.COOKIES)
     
     response = render(request, "home.html")
-    response.set_cookie("Abdulaziz", "The best Thinker")
+    response.set_cookie("dark", "mode", max_age=3500)
 
     return response
 
 def properties_view(request:HttpRequest):
-    return render(request, "properties.html")
+    properties = [
+        {"title" : "Villa Modern in Malqa", "image" : "villa1.jpg"},
+        {"title" : "Great home for you in Rimal", "image" : "villa2.jpg"},
+        {"title" : "Villa with 8 bedrooms in Swedey", "image" : "villa3.jpg"},
+        {"title" : "Amazing Villa in Hitten", "image" : "villa4.jpg"}
+    ]
+
+    return render(request, "properties.html", context={"properties":properties})
 
 def contact_view(request:HttpRequest):
+    
     return render(request, "contact.html")
 
 
