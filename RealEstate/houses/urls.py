8804d51b59ea5548cbbf9c06ff343318856adecf
D. F <ifonei@hotmail.com>
2024-07-14 13:25:54 +0300

    bug fixed

--- RealEstate/houses/urls.py
+++ RealEstate/houses/urls.py
@@ -5,8 +5,8 @@ appname = 'houses'
 urlpatterns = [
     path('',views.home, name = 'home'),
     path('properties/',views.properties, name= 'properties'),
-    path('darkModeView',views.darkModeView, name= 'darkModeView'),
-    path('lightModeView',views.lightModeView, name= 'lightModeView'),
+    path('darkModeView/',views.darkModeView, name= 'darkModeView'),
+    path('lightModeView/',views.lightModeView, name= 'lightModeView'),
 ]
 
 
